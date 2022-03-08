47c0672d708d3055e93602287f552fede1ed9fe6
JulioAvalos <julioavalos2@msn.com>
2022-03-07 18:16:43 -0600

    Agregando props y controles a nuestra historia y componente

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,9 +1,17 @@
 import './mylabel.css';
 
-export const MyLabel = () => {
+export interface MyLabelProps {
+  label: string;
+  size: 'normal' | 'h1' | 'h2' | 'h3';
+}
+
+export const MyLabel = ({
+  label = 'No Label', 
+  size = 'normal'
+}: MyLabelProps) => {
   return (
-    <span>
-        Hola mundo
+    <span className={`${ size }`}>
+        {label}
     </span>
   )
 }
--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -1,14 +1,18 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { MyLabel } from '../../components/MyLabel';
 
 
 export default {
   title: 'UI/Etiquetas/MyLabel',
   component: MyLabel,
-};
+} as ComponentMeta<typeof MyLabel>
 
-const Template = () => <MyLabel />;
+const Template: ComponentStory<typeof MyLabel> = (args) => <MyLabel {...args}/>;
 
 export const Basic = Template.bind({});
+Basic.args = {
+  size: 'normal'
+}
 
 export const AllCaps = Template.bind({});
 
